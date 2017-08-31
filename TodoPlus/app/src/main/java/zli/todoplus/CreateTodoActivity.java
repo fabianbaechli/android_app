1f5ba57148429a34cf3f36679c115c281411416d
Fabian Bächli <fb@spline.ch>
2017-08-31 20:15:46 +0200

    removing fragments works

--- TodoPlus/app/src/main/java/zli/todoplus/CreateTodoActivity.java
+++ TodoPlus/app/src/main/java/zli/todoplus/CreateTodoActivity.java
@@ -5,33 +5,33 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import java.util.List;
+import java.util.ArrayList;
 
 public class CreateTodoActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
+    ArrayList<android.app.Fragment> fragments = new ArrayList<>();
     FragmentManager fragmentManager = getFragmentManager();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            removeActiveCenterFragments();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_dashboard:
                     NormalTodoFragment hello = new NormalTodoFragment();
                     fragmentTransaction.add(R.id.fragment_normal, hello, "HELLO");
                     fragmentTransaction.commit();
+                    fragments.add(hello);
                     return true;
                 case R.id.navigation_notifications:
                     SportTodoFragment sportTodoFragment = new SportTodoFragment();
                     fragmentTransaction.add(R.id.fragment_normal, sportTodoFragment, "HELLO");
+                    fragments.add(sportTodoFragment);
                     fragmentTransaction.commit();
             }
             return false;
@@ -48,6 +48,17 @@ public class CreateTodoActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         NormalTodoFragment hello = new NormalTodoFragment();
         fragmentTransaction.add(R.id.fragment_normal, hello, "HELLO");
+        fragments.add(hello);
         fragmentTransaction.commit();
+    }
+    private void removeActiveCenterFragments() {
+        if (fragments.size() > 0) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            for (Fragment activeFragment : fragments) {
+                fragmentTransaction.remove(activeFragment);
+            }
+            fragments.clear();
+            fragmentTransaction.commit();
+        }
     }
 }
