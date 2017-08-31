689cb9d7c8c48ed06e13bebdb9ed9e07752c6225
Unknown <keller.yvo@gmail.com>
2017-08-31 16:21:16 +0200

    implemented list with buttons example

---
+++ ItemToList/app/src/main/java/collective/frdm/yvokeller/itemtolist/ListViewDemo.java
@@ -0,0 +1,39 @@
+package collective.frdm.yvokeller.itemtolist;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yvokeller on 31.08.17.
+ */
+
+public class ListViewDemo extends ListActivity {
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems = new ArrayList<>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+    int clickCounter = 0;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
+    }
+
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        listItems.add("Clicked : " + clickCounter++);
+        adapter.notifyDataSetChanged();
+    }
+}
---
+++ ItemToList/app/src/main/java/collective/frdm/yvokeller/itemtolist/MainActivity.java
@@ -0,0 +1,93 @@
+package collective.frdm.yvokeller.itemtolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnAdd;
+    EditText editTxt;
+    ListView list;
+
+    SimpleAdapter adapter2;
+
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
+
+    private List<Map<String, String>> data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Vars
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        list = (ListView) findViewById(R.id.list);
+        editTxt = (EditText) findViewById(R.id.editText);
+        arrayList = new ArrayList<>();
+
+        /*
+        // Adapter: You need three parameters 'the context, id of the layout (it will be where the data is shown),
+        // and the array that contains the data
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.two_line_list_item, arrayList);
+
+        // Here, you set the data in your ListView
+        list.setAdapter(adapter);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // this line adds the data of your EditText and puts in your array
+                arrayList.add(editTxt.getText().toString());
+                // next thing you have to do is check if your adapter has changed
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        //Verison TWO
+        data = new ArrayList<>();
+
+        adapter2 = new SimpleAdapter(this, data,
+                android.R.layout.simple_list_item_2,
+                new String[] {"First Line", "Second Line" },
+                new int[] {android.R.id.text1, android.R.id.text2 });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, String> item = new HashMap<>(2);
+                item.put("First Line", editTxt.getText().toString());
+                item.put("Second Line","Second line of text");
+                data.add(item);
+
+                adapter2.notifyDataSetChanged();
+            }
+        });
+
+        list.setAdapter(adapter2);
+        */
+
+        //generate list
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("item1");
+        list.add("item2");
+
+        //instantiate custom adapter
+        MyCustomAdapter adapter = new MyCustomAdapter(list, this);
+
+        //handle listview and assign adapter
+        ListView lView = (ListView)findViewById(R.id.list);
+        lView.setAdapter(adapter);
+    }
+}
