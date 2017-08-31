152aa3de1940df0981ddab0b5eb6a2d421d53225
Fabian Bächli <fb@spline.ch>
2017-08-31 14:33:35 +0200

    commit before switching branch

--- TodoPlus/app/src/main/java/zli/todoplus/AddTodoActivity.java
+++ TodoPlus/app/src/main/java/zli/todoplus/AddTodoActivity.java
@@ -2,6 +2,9 @@ package zli.todoplus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class AddTodoActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class AddTodoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_todo);
         setTitle("Add Todo");
+        final Switch sportTodo = (Switch) findViewById(R.id.sportTodoToggle);
+        final Switch remindMe = (Switch) findViewById(R.id.remindmeToggle);
+        final Switch priorityTodo = (Switch) findViewById(R.id.priorityTodoToggle);
+        sportTodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sportTodo.isChecked()) {
+                    remindMe.setClickable(false);
+                    priorityTodo.setClickable(false);
+                } else {
+                    remindMe.setClickable(true);
+                    priorityTodo.setClickable(true);
+                }
+
+            }
+        });
+
+        remindMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        priorityTodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
