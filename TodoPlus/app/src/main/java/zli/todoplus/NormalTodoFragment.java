bcc997210b0b18d2d6e17e21c8b6963b90c51307
Fabian Bächli <fb@spline.ch>
2017-09-03 14:06:58 +0200

    Getting click events in fragment works

--- TodoPlus/app/src/main/java/zli/todoplus/NormalTodoFragment.java
+++ TodoPlus/app/src/main/java/zli/todoplus/NormalTodoFragment.java
@@ -5,47 +5,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Switch;
-import android.widget.ToggleButton;
 
-public class NormalTodoFragment extends Fragment {
-    private View.OnClickListener handleDateToggle = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            final Switch dateToggle = getView().findViewById(R.id.remindMeOnADayToggle);
-            dateToggle.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    System.out.println(dateToggle.isChecked());
-                }
-            });
-        }
-    };
+public class NormalTodoFragment extends Fragment implements View.OnClickListener{
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        /*
-        final Switch dateToggle = getView().findViewById(R.id.priorityTodoToggle);
-        dateToggle.setOnClickListener(handleDateToggle);
-        */
-        return inflater.inflate(R.layout.fragment_normal, null);
-    }
-    /*
-    @Override
-    public void onHiddenChanged(boolean hidden) {
-        System.out.println("hello");
-        super.onHiddenChanged(hidden);
-        if (!hidden) {
-            System.out.println("skeeet");
-            final Switch dateToggle = getView().findViewById(R.id.remindMeOnADayToggle);
+        View view = inflater.inflate(R.layout.fragment_normal, container, false);
+        // The three clickable elements in the fragment
+        Switch reminderToggle = view.findViewById(R.id.remindMeOnADayToggle);
+        Switch priorityToggle = view.findViewById(R.id.priorityTodoToggle);
+        Button setReminderButton = view.findViewById(R.id.setReminderButton);
 
-            dateToggle.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    System.out.println(dateToggle.isChecked());
-                }
-            });
+        // Sets the click listener for the elements, which calls onClick in this class
+        priorityToggle.setOnClickListener(this);
+        reminderToggle.setOnClickListener(this);
+        setReminderButton.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            // Reminder Toggle event
+            case R.id.remindMeOnADayToggle:
+                System.out.println("reminder toggle");
+                break;
+            // Priority Toggle event
+            case R.id.priorityTodoToggle:
+                System.out.println("priority toggle");
+                break;
         }
     }
-    */
 }
