96d4672aa7bf2b3b5cc42edc205e0eeeccb85679
Fabian Bächli <fb@spline.ch>
2017-09-06 13:59:24 +0200

    Got a notification!

--- TodoPlus/app/src/main/java/zli/todoplus/notification/MyReceiver.java
+++ TodoPlus/app/src/main/java/zli/todoplus/notification/MyReceiver.java
@@ -7,27 +7,14 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 
-public class MyReceiver extends BroadcastReceiver implements ServiceConnection {
+public class MyReceiver extends BroadcastReceiver {
     Intent notifyIntent;
-    Context context;
-
     public MyReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         notifyIntent = new Intent(context, MyNewIntentService.class);
-        this.context = context;
-        context.bindService(notifyIntent, this, Context.BIND_AUTO_CREATE);
-    }
-
-    @Override
-    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
         context.startService(notifyIntent);
-    }
-
-    @Override
-    public void onServiceDisconnected(ComponentName componentName) {
-
     }
 }
